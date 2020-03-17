048fe9437be8c4cbd0773bb3996984302de428fe
minhaz <minhaz.taher@gmail.com>
2020-03-18 00:32:14 +0600

    gmail loig in firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./firebase.config";
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: "",
+    photo: "",
+    email: ""
+  });
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(res => {
+        const { displayName, photoURL, email } = res.user;
+        const signInUser = {
+          isSignedIn: true,
+          name: displayName,
+          photo: photoURL,
+          email: email
+        };
+        setUser(signInUser);
+      })
+      .catch(err => {
+        console.log(err);
+        console.log(err.message);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleSignIn}>Sign In</button>
+      {user.isSignedIn && (
+        <div>
+          {" "}
+          <p>Welcome {user.name}!</p>
+          <p>Email : {user.email}</p>
+          <img src={user.photo} alt="pic" />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDQ2uGRAAl-io9v3UmlwVgnQv-IR17vFjo",
+  authDomain: "fir-auth-29641.firebaseapp.com",
+  databaseURL: "https://fir-auth-29641.firebaseio.com",
+  projectId: "fir-auth-29641",
+  storageBucket: "fir-auth-29641.appspot.com",
+  messagingSenderId: "47951670316",
+  appId: "1:47951670316:web:56b3693ec9b9d43ddb7589"
+};
+
+export default firebaseConfig;
